4e39bf3473b3f3662753fe5c83e32a42ac7d1e5f
unknown <marina.iovbak@gmail.com>
2019-04-03 12:28:37 +0600

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,15 +16,16 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
 	console.log("New user connected");
 
-	// socket.emit("newEmail", {
-	// 	from: "marina.iovbak@example.com",
-	// 	text: "Hey. What is going on",
-	// 	createAt: 123
-	// });
+	socket.emit("newMessage", {
+		from: "Admin",
+		text: "Welcome to the chat app"
+	});
 
-	// socket.on("createEmail", (newEmail) => {
-	// 	console.log("createEmail", newEmail);
-	// });
+	socket.broadcast.emit("newMessage", {
+		from: "Admin",
+		text: "New user joined",
+		createAt: new Date().getTime()
+	})
 
 	socket.on("createMessage", (message) => {
 		console.log("createMessage", message);
@@ -33,6 +34,12 @@ io.on("connection", (socket) => {
 			text:message.text,
 			createAt: new Date().getTime()
 		});
+
+		// socket.broadcast.emit("newMessage", {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createAt: new Date().getTime()
+		// });
 	});
 
 	socket.on("disconnect", () => {
